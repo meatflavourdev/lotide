87d166c0a7fadab115a5be4df1597ec00087f365
Jeremy Felix D <me@jeremyfelix.com>
2020-10-26 18:18:15 -0700

    Implements function assertEqual() with test cases

--- assertEqual.js
+++ assertEqual.js
@@ -1,1 +1,28 @@
-test
+// Function Implementation
+const assertEqual = function(actual, expected) {
+  if(actual === expected){
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} === ${expected}`);
+  }
+};
+
+// Test Code
+assertEqual("String Identical", "String Identical");
+assertEqual("abc", "abc");
+assertEqual(1, 1);
+assertEqual(10.0, 10.0);
+assertEqual(12e3, 12e3);
+assertEqual(true, true);
+assertEqual(undefined, undefined);
+
+
+assertEqual("String Not Identical", "String");
+assertEqual("abc", "def");
+assertEqual("abc", "123");
+assertEqual(1, 10);
+assertEqual(-45, 1.44);
+assertEqual(12e3, 12e-3);
+assertEqual(12e3, NaN);
+assertEqual(true, false);
+assertEqual(NaN, NaN);
