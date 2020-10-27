cbfa7f4ec0755fb8258533a09af292ea8d511e46
Jeremy Felix D <me@jeremyfelix.com>
2020-10-26 18:39:48 -0700

    assertEqual.js jdDoc comment

--- assertEqual.js
+++ assertEqual.js
@@ -1,4 +1,8 @@
-// Function Implementation
+/**
+ * Logs message to the console remarking on the equality of the specified parameters
+ * @param  {*} actual The value to test against
+ * @param  {*} expected The value to be tested
+ */
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
