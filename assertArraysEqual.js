9019a1759786abe1eb13bc0077f2d42b89ba9099
Jeremy Felix D <me@jeremyfelix.com>
2020-10-28 15:07:10 -0700

    Add jsdoc to assertArrayEquals()

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -15,6 +15,12 @@ const eqArrays = function(actual, expected) {
   return true;
 };
 
+
+/**
+ * Logs a message to the console inidicating if the two shallow arrays passes in are identical
+ * @param  {*[]} actual The array to compare
+ * @param  {*[]} expected The array to compare against
+ */
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
     console.log(`🟢 Assertion Passed: [${actual}] === [${expected}]`);
