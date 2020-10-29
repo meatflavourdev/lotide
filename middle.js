8023f50a2e8965fb568a8e7d5fbfddcbacce4398
Jeremy Felix D <me@jeremyfelix.com>
2020-10-28 18:31:44 -0700

    middle.js lint fixes

--- middle.js
+++ middle.js
@@ -47,10 +47,10 @@ const assertEqual = function(actual, expected) {
  * @return {*[]} An array containing the middle element(s) if they exist
  */
 const middle = function(arr) {
-  if(!Array.isArray(arr)) return false;
-  if(arr.length < 3) return [];
-  if(arr.length % 2 === 1) return [arr[Math.floor(arr.length / 2)]];
-  if(arr.length % 2 === 0) return [arr[arr.length / 2 - 1], arr[arr.length / 2]];
+  if (!Array.isArray(arr)) return false;
+  if (arr.length < 3) return [];
+  if (arr.length % 2 === 1) return [arr[Math.floor(arr.length / 2)]];
+  if (arr.length % 2 === 0) return [arr[arr.length / 2 - 1], arr[arr.length / 2]];
 };
 
 assertEqual(middle(undefined), false); // => false
