696dd7613e9c063c8f6a438667cad610650e8818
Jeremy Dombrowski <dev@jeremyfelix.com>
2020-11-02 15:20:43 -0800

    Convert head tests to mocha/chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,48 +1,60 @@
-const assertEqual = require('../assertEqual');
-const head = require('../head');
+const assert = require('chai').assert;
+const head   = require('../head');
 
-// Test Cases -----------------------------------
+describe("#head", () => {
+  it("Returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
 
-// Empty Array
-console.log(`Input: ${[]}`);
-assertEqual(head([]), undefined);
+  it("Returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
 
-// Array with single number
-console.log(`Input: ${[1]}`);
-assertEqual(head([1]), 1);
+  it("Returns undefined when passed and empty array", () => {
+    assert.strictEqual(head([]), undefined); 
+  });
 
-// Array with single boolean
-console.log(`Input: ${[true]}`);
-assertEqual(head([true]), true);
+  it("Returns the single number when passes a single number", () => {
+    assert.strictEqual(head([1]), 1); 
+  });
 
-// Array with numbers
-console.log(`Input: ${[5,6,7]}`);
-assertEqual(head([5,6,7]), 5);
+  it("Returns the single boolean when passes a single boolean", () => {
+    assert.strictEqual(head([true]), true); 
+  });
 
-// Array with strings
-console.log(`Input: ${["Hello", "Lighthouse", "Labs"]}`);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  it("Returns the first element when passes an array of numbers", () => {
+    assert.strictEqual(head([5,6,7]), 5); 
+  });
 
-// Array with booleans
-console.log(`Input: ${[true, false, true, true, false]}`);
-assertEqual(head([true, false, true, true, false]), true);
+  it("Returns the first element when passes an array of strings", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); 
+  });
 
-// Array with mixed types
-console.log(`Input: ${["334 23", false, "sdfs", true, "fooo", 343]}`);
-assertEqual(head(["334 23", false, "sdfs", true, "fooo", 343]), "334 23");
+  it("Returns the first element when passes an array of booleans", () => {
+    assert.strictEqual(head([true, false, true]), true); 
+  });
+  
+  it("Returns the first element when passes an array of mixed types", () => {
+    assert.strictEqual(head(["334 23", false, "sdfs", true, "fooo", 343]), "334 23"); 
+  });
+  
+  it("Returns undefined when passed an empty object", () => {
+    assert.strictEqual(head({}), undefined); 
+  });
 
-// Empty Object
-console.log(`Input: ${{}}`);
-assertEqual(head({}), undefined);
+  it("Returns undefined when passed an non-empty object", () => {
+    assert.strictEqual(head({3: 3, "string": "value", key: true, that: undefined, nullVal: null}), undefined); 
+  });
 
-// Empty String
-console.log(`Input: ${""}`);
-assertEqual(head(""), undefined);
+  it("Returns undefined when passed a number", () => {
+    assert.strictEqual(head(12), undefined); 
+  });
 
-// String (Weird case)
-console.log(`Input: ${"this is a string"}`);
-assertEqual(head("this is a string"), "t");
+  it("Returns undefined when passed an empty string", () => {
+    assert.strictEqual(head(""), undefined); 
+  });
 
-// Number
-console.log(`Input: ${12}`);
-assertEqual(head(12), undefined);
+  it("Returns first character when passed a string", () => {
+    assert.strictEqual(head("this is a string"), "t"); 
+  });
+});
