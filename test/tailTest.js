a7241664d8ddf68132fcbea070e2d4a5530e356a
Jeremy Dombrowski <dev@jeremyfelix.com>
2020-11-02 17:08:42 -0800

    Convert tail tests to mocha/chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,31 +1,39 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-// Test Case 1: Check the returned array elements
-console.log(`Test 1`);
-const test1 = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(test1.length, 2); // ensure we get back two elements
-assertEqual(test1[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(test1[1], "Labs"); // ensure second element is "Labs"
+describe("#head", () => {
+  it("Returns an array of length 2 when passed an array of length 3", () => {
+    const test1 = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.strictEqual(test1.length, 2);
+  });
 
-// Test Case 2: Check the original array
-console.log(`Test 2`);
-const test2 = ["Yo Yo", "Lighthouse", "Labs"];
-tail(test2); // no need to capture the return value since we are not checking it
-assertEqual(test2.length, 3); // original array should still have 3 elements!
+  it("Returns an array containing the last two elements", () => {
+    const testArray = ["Hello", "Lighthouse", "Labs"];
+    const test1 = tail(testArray);
+    assert.deepEqual(test1, ["Lighthouse", "Labs"]);
+  });
 
-// Test Case 3: Array with one element
-console.log(`Test 3`);
-const test3 = tail(["Hello"]);
-assertEqual(test3.length, 0); // Ensure returned array has length of 0
+  it("Does not modify the origial array but instead makes a shallow copy", () => {
+    const test2 = ["Yo Yo", "Lighthouse", "Labs"];
+    tail(test2); 
+    assert.deepEqual(test2, ["Yo Yo", "Lighthouse", "Labs"]); 
+  });
+  
+  it("Returns empty array when passed array of single element", () => {
+    const test3 = ["Lonely"];
+    const result3 = tail(test3);
+    assert.deepEqual(result3, [],);
+  });
 
-// Test Case 4: Empty Array
-console.log(`Test 4`);
-const test4 = [];
-tail(test4);
-assertEqual(test4.length, 0); // Ensure returned array has length of 0
+  it("Returns empty array when passed empty array", () => {
+    const test4 = [];
+    const result4 = tail(test4);
+    assert.deepEqual(result4, []);
+  });
 
-// Test Case 5: Array with one undefined element
-console.log(`Test 5`);
-const test5 = tail([undefined]);
-assertEqual(test5.length, 0); // Ensure returned array has length of 0
+  it("Returns empty array when passed array of undefined element", () => {
+    const test5 = [undefined];
+    const result5 = tail(test5);
+    assert.deepEqual(result5, []); 
+  });
+});
